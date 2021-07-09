495abc430a27edd1a19f765b50af0a102e7810ff
raiduy <radu.apsan@gmail.com>
2021-07-09 22:44:14 +0200

    Cards component added and landing page almost done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import './App.scss';
 import Navbar from './Navbar.js'
+import Cards from './Cards.js';
 
 function App() {
   return (
     <div class="application">
       <div class="application-content">
-        <h1 class="glitch" data-text="Pick">
-          Pick
+        <h1>
+          Hello there
         </h1>
+        <h2>
+          Pick your poison
+        </h2>
+        <Cards />
       </div>
     </div>
   );
---
+++ src/components/Cards.js
@@ -0,0 +1,24 @@
+import './Cards.css'
+
+function Cards() {
+  return(
+    <div class="cards-container">
+      <div class="card coding">
+        <h2>Coding</h2>
+        <p>Have a look into my programming skills.</p>
+      </div>
+
+      <div class="card media">
+        <h2>Media</h2>
+        <p>Enjoy a trip though my art journey with photos, videos and more.</p>
+      </div>
+
+      <div class="card about">
+        <h2>About Me</h2>
+        <p>Get to know me and what I do.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
