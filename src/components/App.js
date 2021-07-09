8fb68715ba76abb82cec8684099607ed8fbc760e
raiduy <radu.apsan@gmail.com>
2021-07-09 19:12:06 +0200

    Landing page title and navbar component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Navbar from './Navbar.js'
+
+function App() {
+  return (
+    <div class="application">
+      <div class="application-content">
+        <h1 class="glitch" data-text="Pick">
+          Pick
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div class="topnav">
+      <a id="home" href="#home">Radu Apsan</a>
+      <a id="about" href="#about">.about</a>
+      {/* <a id="blend" href="#blender">.blend</a>
+      <a id="mp4" href="#video">.mp4</a> */}
+      <a id="jpg" href="#photo">.media</a>
+      <a id="code" href="#code">.code()</a>
+    </div>
+  );
+}
+
+export default Navbar; 
