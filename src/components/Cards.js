d2cf70111e7eb6891ab31ec7390da53f0c5b1f4e
raiduy <radu.apsan@gmail.com>
2021-07-10 19:04:01 +0200

    added svgs and border shadows

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,25 @@
 import './Cards.css'
+import code from '../images/coding.svg'
+import camera from '../images/camera.svg'
+import me from '../images/me.svg'
 
 function Cards() {
   return(
     <div class="cards-container">
       <div class="card coding">
+        <img src={code} alt="Code image from" />
         <h2>Coding</h2>
         <p>Have a look into my programming skills.</p>
       </div>
 
       <div class="card media">
+        <img src={camera} alt="Camera image from" />
         <h2>Media</h2>
         <p>Enjoy a trip though my art journey with photos, videos and more.</p>
       </div>
 
       <div class="card about">
+        <img src={me} alt="Person image from" />
         <h2>About Me</h2>
         <p>Get to know me and what I do.</p>
       </div>
