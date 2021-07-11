3f76ee82f3f136733838317dc4ea7fff0e1ff433
raiduy <radu.apsan@gmail.com>
2021-07-12 01:43:08 +0200

    cards responsive

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,20 +8,26 @@ function Cards() {
   return(
       <div class="cards-container">
         <Link to='/code' className="card coding">
-          <img src={code} alt="Code"/>
-          <h2>Coding</h2>
+          <div class="inner-row">
+            <img src={code} alt="Code"/>
+            <h2>Coding</h2>
+          </div>
           <p>Have a look into my programming skills.</p>
         </Link>
 
         <Link to='/media' className="card media">
-          <img src={camera} alt="Media"/>
-          <h2>Media</h2>
-          <p>Enjoy a trip though my art journey with photos, videos and more.</p>
+          <div class="inner-row">
+            <img src={camera} alt="Media"/>
+            <h2>Media</h2>
+          </div>
+          <p>Enjoy a trip though my art journey.</p>
         </Link>
 
         <Link to='/about' className="card about">
-          <img src={me} alt="About"/>
-          <h2>About Me</h2>
+          <div class="inner-row">
+            <img src={me} alt="About"/>
+            <h2>About Me</h2>
+          </div>
           <p>Get to know me and what I do.</p>
         </Link>
       </div>
